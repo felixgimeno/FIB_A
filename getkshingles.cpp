d009ccea6eed0ecdc95ba8380c0c97485e174a01
Hack Athon <hack64@athon.es>
2016-10-30 18:47:05 +0100

    added jaccard similarity

--- getkshingles.cpp
+++ getkshingles.cpp
@@ -18,6 +18,44 @@ k_shingles get_k_shingles_from_string(const size_t k, const string str){
 	return ret;
 }
 
+double jaccard_similarity(const k_shingles A, const k_shingles B){
+	k_shingles C = B;
+	uint_fast64_t count_intersection = 0;
+	uint_fast64_t count_union = 0;
+	for (const auto& v : A){
+		const auto it = C.find(v);
+		const bool t = it == C.end(); 
+		if (t){
+			// v is in A, isn't in B
+			count_intersection += 0;
+			count_union += 1;
+		}
+		else {
+			// v is in A, is in B
+			count_intersection += 1;
+			count_union += 1;
+			// we remove from our copy of B 
+			// the intersection with A
+			C.erase(it);			
+		}
+	}
+	count_union += C.size();
+	C.clear();
+	return double(count_intersection)/double(count_union);	
+}
+
+void print(const k_shingles A){for (const auto& v : A){cout << "\"" << v << "\", ";} cout << endl;}
+
 int main(void){
+	const size_t k = 9;
+	const string str1 = "minhashing is the best";
+	const string str2 = "minhashing is not the best";
+	const k_shingles A = get_k_shingles_from_string(k, str1);
+	const k_shingles B = get_k_shingles_from_string(k, str2);
+	print(A);
+	print(B);
+	const double j_sim = jaccard_similarity(A, B);
+	cout << j_sim << endl;
 	return 0;
+	
 } 
